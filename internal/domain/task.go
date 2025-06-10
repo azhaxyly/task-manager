a7cbaf214e7c6bee2ad35b4bb0c96855c3b3d349
Azamat Zhaxylykov <a19732805@gmail.com>
2025-06-10 08:34:18 +0500

    feat: task model ready

--- internal/domain/task.go
+++ internal/domain/task.go
@@ -1,3 +1,23 @@
 package domain
 
+import "time"
+
 type TaskID string
+
+type Task struct {
+	ID         TaskID
+	CreatedAt  time.Time
+	StartedAt  *time.Time
+	FinishedAt *time.Time
+	Status     Status
+	Result     *string
+	Err        *string
+}
+
+func NewTask(id TaskID) *Task {
+	return &Task{
+		ID:        id,
+		CreatedAt: time.Now().UTC(),
+		Status:    Pending,
+	}
+}
