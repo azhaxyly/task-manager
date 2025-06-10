1e0bef200ac07b6868ebee2923ad93b3a19f7ffd
Azamat Zhaxylykov <a19732805@gmail.com>
2025-06-10 09:11:49 +0500

    fix: delete trash ideas from interfaces

--- internal/application/port/out/taskrepo.go
+++ internal/application/port/out/taskrepo.go
@@ -6,7 +6,6 @@ import (
 )
 
 type TaskRepository interface {
-	Add(ctx context.Context, task *domain.Task) error
 	Save(ctx context.Context, t *domain.Task) error
 	Find(ctx context.Context, id domain.TaskID) (*domain.Task, error)
 	Delete(ctx context.Context, id domain.TaskID) error
--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -3,9 +3,6 @@ package domain
 import "context"
 
 type TaskRepository interface {
-	// добавялет таску в репу
-	Add(ctx context.Context, task *Task) error
-
 	// сохраняет либо обновляет таску
 	Save(ctx context.Context, task *Task) error
 
@@ -14,7 +11,4 @@ type TaskRepository interface {
 
 	// удаялет таску
 	Delete(ctx context.Context, id TaskID) error
-
-	// // ищет таски по фильтру
-	// FindByFilter(ctx context.Context, filter TaskFilter) ([]*Task, error)
 }
