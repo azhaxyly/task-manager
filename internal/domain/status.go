f44bb767b983e1964568738e64f8e3f35d65c006
Azamat Zhaxylykov <a19732805@gmail.com>
2025-06-10 08:27:35 +0500

    init: project arch and base domain

---
+++ internal/domain/status.go
@@ -0,0 +1,32 @@
+package domain
+
+type Status string
+
+const (
+	// таск создан, но пока не запущен
+	Pending Status = "pending"
+	// таск запущен и выполняется
+	Running Status = "running"
+	// таск завершился успешно
+	Success Status = "completed"
+	// таск завершился с ошибкой
+	Failed Status = "failed"
+	// таск отменен
+	Canceled Status = "canceled"
+)
+
+var AllStatuses = []Status{
+	Pending,
+	Running,
+	Success,
+	Failed,
+	Canceled,
+}
+
+func (s Status) IsValid() bool {
+	switch s {
+	case Pending, Running, Success, Failed, Canceled:
+		return true
+	}
+	return false
+}
---
+++ internal/domain/task.go
@@ -0,0 +1,3 @@
+package domain
+
+type TaskID string
